05b6ea68f33be693ba2d8d8bc27fa892a8797425
xd9642 <danielmckenna93@gmail.com>
2022-05-29 21:53:26 +0100

    Add Watcher service and create demo project

--- cv-subscriber/src/server.rs
+++ cv-subscriber/src/server.rs
@@ -1,6 +1,10 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::sync::mpsc;
 
+// Thoughts we want some sort of image interner so we can make it simple to register a single image
+// and show where that image is used in operations. Otherwise for every op if we have to provide
+// the inputs and outputs and transmit them again that is _pain_. But also, can we do this in a way
+// that's actually nice for the user to use?
 pub struct ImageServer {
     incoming: mpsc::Receiver<()>,
 }
